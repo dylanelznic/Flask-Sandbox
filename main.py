5c619b0b88932add88a508d6ad863c5f6d3463ca
Dylan Elznic <djey96@mst.edu>
2017-07-11 14:10:58 -0500

    Defined Landing and Dashboard Views/Templates

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from dummy_script import print_to_console
 
 app = Flask(__name__)
 
 @app.route('/')
-def landing_page():
-	return 'Hello'
+def landing():
+	return render_template('landing.html')
+
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
 
 if __name__ == "__main__":
 	app.run()
